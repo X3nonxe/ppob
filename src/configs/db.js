5120eacfef1f668dfccd04227938b8eabca6aaea
rohmanahmadtaufiq-ux <rohmanahmadtaufiq@gmail.com>
2025-10-30 19:38:27 +0700

    feat: refactor database configuration to use a single DATABASE_URL environment variable

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   max: 10,
   min: 0,
   idleTimeoutMillis: 8000,
